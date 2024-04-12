2737ad48107d2d3eb63dc1ca8351fb3abe245b67
niwat <Niwat.s@unii.co.th>
2024-04-12 18:00:14 +0700

    ฺฺCommit before Songkran

---
+++ app/src/main/java/com/aa/project2/test/TestLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.aa.project2.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aa.project2.R
+
+class TestLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_layout)
+    }
+}
